72c7735fc930d2ab06c67bf0d087d3162782c138
singleufo <ghoshabhirup2001@gmail.com>
2023-05-08 12:59:20 +0530

    test - sending data to subdomain

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import Item from "./Components/Item";
 
 function App() {
+  useEffect(() => {
+    const iframe = document.querySelector("iframe");
+    const iframeWindow = iframe.contentWindow;
+
+    const data = {
+      name: "Abhirup",
+      company: "Perch",
+    };
+
+    iframeWindow.postMessage(JSON.stringify(data), "*");
+    // console.log(iframeWindow);
+  }, []);
+
   return (
     <div>
       <Item />
+      <iframe
+        src="https://perch-assignment-abhirup.netlify.app/"
+        title="Destination"
+        style={{ display: "none" }}
+      ></iframe>
     </div>
   );
 }
